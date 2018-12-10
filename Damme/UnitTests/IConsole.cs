0b014d796e51a3f812f6e55c87a1ed2e7670b93d
A.R <richardadrien0@gmail.com>
2018-12-10 15:58:28 +0100

    mod

---
+++ Damme/UnitTests/IConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace Damme
+{
+    public interface IConsole
+    {
+        /// <summary>
+        /// Reading from the console method
+        /// </summary>
+        /// <returns></returns>
+        string ReadLine();
+
+        /// <summary>
+        /// Writing in console method.
+        /// </summary>
+        /// <param name="outputString"></param>
+        void WriteLine(string outputString);
+
+        /// <summary>
+        /// Writing messages in teh error console.
+        /// </summary>
+        /// <param name="outputString"></param>
+        void WriteErrorLine(string outputString);
+    }
+}
---
+++ Damme/UnitTests/TrueConsole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Damme
+{
+    public class TrueConsole : IConsole
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WriteLine(string outputString)
+        {
+            Console.WriteLine(outputString);
+        }
+
+        public void WriteErrorLine(string outputString)
+        {
+            Console.Error.WriteLine(outputString);
+        }
+    }
+}
